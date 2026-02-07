1d16436599a63e84122d110a0aac104a86efff7f
GozdeEBaran <eski.gozdee@gmail.com>
2026-02-06 20:13:41 -0500

    Initial Commit

---
+++ QuietSpace/QuietSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietSpaceApp.swift
+//  QuietSpace
+//
+//  Created by Gozde Eski on 2026-02-06.
+//
+
+import SwiftUI
+
+@main
+struct QuietSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
